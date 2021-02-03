684040a8b552a495514e74043ec07a17eb341fde
Fionnie Pollack <fionniepollack@gmail.com>
2021-02-03 05:23:01 +0000

    Finish passing form data and append to page

--- profile.py
+++ profile.py
@@ -12,15 +12,32 @@ def index():
 
 
 
-@app.route('/profile', methods=['POST'])
+@app.route('/api/profile', methods=['POST'])
 def profile():
     """Return results from profile form."""
 
     fullname = request.form['name']
     # TODO: get the values from the rest of the form
     # Add them to jsonify
-    
-    return jsonify({'fullname': fullname, })
+    age = request.form['age']
+    occupation = request.form['occupation']
+    salary = request.form['salary']
+    education = request.form['education']
+    state = request.form['state']
+    city = request.form['city']
+    garden = request.form['garden']
+    tv = request.form['tv']
+
+    return jsonify({'fullname': fullname, 
+                    'age': age,
+                    'occupation': occupation,
+                    'salary': salary,
+                    'education': education,
+                    'state': state,
+                    'city': city,
+                    'garden': garden,
+                    'tv': tv
+                    })
 
 
 
--- static/profile.js
+++ static/profile.js
@@ -5,12 +5,24 @@ function submitProfile(evt) {
   const formData = {
     name: $('#name-field').val(),
     age: $('#age-field').val(),
-    occupation: $('#occupation-field').val()
+    occupation: $('#occupation-field').val(),
+    salary: $('#salary-field').val(),
+    education: $('#education-field').val(),
+    state: $('#state-field').val(),
+    city: $('#city-field').val(),
+    garden: $('#garden-field').val(),
+    tv: $('#tv-field').val()
   };
 
   $.post('/api/profile', formData, (response) => {
-    $('#profiles').append(`
-      <li>${response.fullname} the ${response.occupation} is ${response.age}</li>
+    $('#profile').append(`
+      <li>${response.fullname} the ${response.occupation} is ${response.age}
+      with salary range ${response.salary}
+      with education level ${response.education} 
+      in state ${response.state} 
+      with city type ${response.city} 
+      with garden answer ${response.garden} 
+      with tv hours ${response.tv}</li>
     `);
   });
 }
